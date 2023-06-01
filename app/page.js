c0ef3956bee8e52ebb2bf50253f9184894b796e2
Saad Khalid <muhammadsaad.khalid@deepchain.pk>
2023-06-01 23:59:09 +0500

    first section done

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
-import Hero from './components/hero';
-import FirstSection from './components/firstSection';
+import Hero from './components/hero/index';
+import FirstSection from './components/firstSection/index';
 
 
 export default function Home() {
